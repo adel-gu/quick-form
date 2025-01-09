d52fdbf0bee43137466ad19a2b509c32981da271
adel <adeelguitoun@gmail.com>
2025-01-09 23:38:21 +0100

    Added field palette side bar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <div class="flex justify-between">
+    <div class="w-[350px] h-screen border-r bg-gray-50">
+      <field-palette />
+    </div>
+    <div class="h-screen"></div>
+    <div class="w-[350px] h-screen border-l bg-gray-50"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FieldPalette from './components/FieldPalette.vue'
+</script>
 
 <style scoped></style>
---
+++ src/components/FieldPalette.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="px-4">
+    <h1 class="text-4xl font-medium my-4">Elements</h1>
+    <div class="h-0.5 mb-8 bg-gray-200" />
+    <div
+      class="flex items-center gap-4 px-4 py-3 hover:shadow-xl hover:cursor-pointer hover:bg-white rounded-md transition-all"
+    >
+      <span
+        class="bg-green-200 flex items-center justify-center p-3 rounded-md text-2xl font-semibold text-green-600"
+        >Aa</span
+      >
+      <div>
+        <p class="text-lg font-bold">Text Input</p>
+        <span class="text-sm">Single text input</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
