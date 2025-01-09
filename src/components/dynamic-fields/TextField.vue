bf2d40e53e15fca1b52db92aea1ca36dc859010e
adel <adeelguitoun@gmail.com>
2025-01-10 00:58:54 +0100

    Added dynamic text field

---
+++ src/components/dynamic-fields/TextField.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- Label -->
+    <label :for="field.id" class="block text-sm font-medium text-gray-700">
+      {{ field.label }} <span v-if="field.required" class="text-red-500">*</span>
+    </label>
+
+    <!-- Input Field -->
+    <input
+      :id="field.id"
+      :name="field.id"
+      :type="field.type"
+      :placeholder="field.placeholder"
+      class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IField } from '@/types/index'
+
+defineProps<{
+  field: IField
+}>()
+</script>
+
+<style lang="scss" scoped></style>
